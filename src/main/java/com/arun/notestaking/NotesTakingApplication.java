699698f1de02fe2561e90b278564e0697754fcc4
ArunJohnJ <jjohnson.arun@gmail.com>
2023-05-23 13:57:27 +0530

    adding .gitignore

---
+++ src/main/java/com/arun/notestaking/NotesTakingApplication.java
@@ -0,0 +1,13 @@
+package com.arun.notestaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesTakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesTakingApplication.class, args);
+	}
+
+}
